76bedca1e8bb4c4a69500affec47dd82cb6eb697
Dankimhaejun <dan.kimhaejun@gmail.com>
2024-02-08 15:17:21 +0900

    feat - Create first example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import './styles.css'
+
+type ParentProps = {
+  children: React.ReactNode
+  lastChild: React.ReactNode
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Parent lastChild={<ChildC />}>
+      <ChildB />
+    </Parent>
+  )
+}
+
+function Parent({ children, lastChild }: ParentProps) {
+  useForceRender(5000)
+  console.log('Parent is rendered')
+
+  return (
+    <div className="parent">
+      <ChildA />
+      {children}
+      {lastChild}
     </div>
   )
+}
+
+function ChildA() {
+  console.log('ChildA is rendered')
+  return <div className="childA">A</div>
+}
+
+function ChildB() {
+  console.log('ChildB is rendered')
+  return <div className="childB">B</div>
+}
+
+function ChildC() {
+  console.log('ChildC is rendered')
+  return <div className="childC">C</div>
+}
+
+function useForceRender(interval: number) {
+  const [tick, setTick] = useState(0)
+  useEffect(() => {
+    const id = setInterval(() => setTick((tick) => tick + 1), interval)
+    return () => clearInterval(id)
+  }, [interval])
+
+  return tick
 }
 
 export default App
